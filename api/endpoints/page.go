3ce8c8a9cb80ba0e95219ef4e055e3e59385aa16
Ales Jandera <ales.jandera@gmail.com>
2023-02-14 17:10:16 +0100

    Update save page.

--- api/endpoints/page.go
+++ api/endpoints/page.go
@@ -137,9 +137,9 @@ func UpdatePage(w http.ResponseWriter, r *http.Request, c utils.ClientData) {
 			return
 		}
 
-		log.Println(page)
+		log.Println(page.Id)
 		response := simplejson.New()
-		c.Db.Model(model.Post{}).Where("id = ?", page.Id).Updates(model.Page{
+		c.Db.Model(&model.Post{}).Where("id = ?", page.Id).Updates(model.Page{
 			Body:        page.Body,
 			Title:       page.Title,
 			MetaTitle:   page.MetaTitle,
